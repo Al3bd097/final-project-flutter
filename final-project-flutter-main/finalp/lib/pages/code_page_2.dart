e894131a19b2a7f21ec8c2b1022e2f2c478ded90
Al3bd097 <al3bdf@gmail.com>
2022-12-12 03:08:37 +0300

    videos page i hope so

--- final-project-flutter-main/finalp/lib/pages/code_page_2.dart
+++ final-project-flutter-main/finalp/lib/pages/code_page_2.dart
@@ -9,19 +9,54 @@ class CodePage2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(codes.name)),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset(
-            codes.image,
-            width: 200,
-          ),
-          Text(codes.name),
-          Text(codes.description)
-        ],
-      )),
+      appBar: AppBar(
+        title: Text(Code.FrontEnds[0].name),
+        backgroundColor: Color(0xff000000),
+      ),
+      body: Container(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(
+              codes.image,
+              width: 180,
+            ),
+            Text(
+              codes.name,
+              style: TextStyle(fontSize: 30, color: Color(0xffe5e5e5)),
+            ),
+            Text(
+              codes.description,
+              style: TextStyle(fontSize: 20, color: Color(0xffe5e5e5)),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Videos",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Color(0xff14213d),
+                    ),
+                  ),
+                  Text(
+                    ">",
+                    style: TextStyle(fontSize: 40),
+                  )
+                ],
+              ),
+              margin: EdgeInsets.only(top: 50, right: 10, left: 10),
+              decoration: BoxDecoration(
+                  color: Color(0xffe5e5e5),
+                  borderRadius: BorderRadius.circular(10)),
+              padding: EdgeInsets.all(15),
+            )
+          ],
+        )),
+        color: Color(0xff14213d),
+      ),
     );
   }
 }
